de06a0ba83a0c8914ae1230da9fe15c0e8606438
Andreas Ekeblad <aekeblad@gmail.com>
2022-05-31 09:37:11 +0200

    setup of project and calculator UI and basic calculator functions made

---
+++ app.js
@@ -0,0 +1,18 @@
+let firstVariable = 0;
+let secondVariable = 0;
+
+function divide(a, b) {
+  return a / b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function subtract(a, b) {
+  return a - b;
+}
+
+function add(a, b) {
+  return a + b;
+}
