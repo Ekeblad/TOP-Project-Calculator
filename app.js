02039beea595dc9729d876d960c2e6b00499b993
Andreas Ekeblad <aekeblad@gmail.com>
2022-06-03 15:50:44 +0200

    major steps taken to improve function

--- app.js
+++ app.js
@@ -1,5 +1,45 @@
-let firstVariable = 0;
-let secondVariable = 0;
+const display = document.querySelector(".screen");
+const buttons = document.querySelectorAll(".button");
+console.log(buttons);
+
+let displayNum = "0";
+let firstNumber = null;
+let secondNumber = null;
+let operator = null;
+let result = null;
+
+function populateDisplay() {
+  display.innerText = displayNum;
+}
+
+//Add event listener
+for (let i = 0; i < buttons.length; i++) {
+  buttons[i].addEventListener("click", function () {
+    clickEvent(buttons[i]);
+  });
+}
+
+//Defined a clickevent which fires functions for numbers, operands and equal signs
+function clickEvent(btnPress) {
+  if (btnPress.classList.contains("number")) {
+    console.log(btnPress.dataset.value);
+  } else if (btnPress.classList.contains("operator")) {
+    console.log(btnPress.dataset.value);
+  } else if (btnPress.classList.contains("equals")) {
+    console.log(btnPress.dataset.value);
+  } else if (btnPress.classList.contains("clear")) {
+    clearDisplay();
+  }
+}
+
+function clearDisplay() {
+  displayNum = "0";
+  firstNumber = null;
+  secondNumber = null;
+  operator = null;
+  result = null;
+  populateDisplay();
+}
 
 function divide(a, b) {
   return a / b;
@@ -15,4 +55,25 @@ function subtract(a, b) {
 
 function add(a, b) {
   return a + b;
+}
+
+function operate(operator, a, b) {
+  a = Number(a);
+  b = Number(b);
+  switch (operator) {
+    case "/":
+      return divide(a, b);
+
+    case "*":
+      return multiply(a, b);
+
+    case "-":
+      return subtract(a, b);
+
+    case "+":
+      return add(a, b);
+
+    default:
+      break;
+  }
 }
